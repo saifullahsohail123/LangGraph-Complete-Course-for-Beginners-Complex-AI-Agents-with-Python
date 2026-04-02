53d1de7674f54824f004e2cbe46e74eef41ec90e
Saif Ullah <saifu0905@gmail.com>
2026-04-02 15:12:30 +0500

    print the anwers and answers result seprately. the answers one is commented out

--- graph2/multiple-inputs.py
+++ graph2/multiple-inputs.py
@@ -13,4 +13,19 @@ def process_values(mystate: AgentState) -> AgentState:
     mystate["result"] = f"Hi there {mystate["name"]}! Your sum  = {sum(mystate["values"])}"
     return mystate
 
-# 
+# Defining the Graph
+
+graph = StateGraph(AgentState)
+
+
+graph.add_node("processor", process_values)
+graph.set_entry_point("processor")
+graph.set_finish_point("processor")
+
+app = graph.compile() # Compiling the graph
+
+
+answers = app.invoke({"name": "Alice", "values": [1, 2, 3]}) # Invoking the graph with multiple inputs
+
+# print(answers) # Printing the entire state after processing
+print(answers["result"]) # Printing the result only
