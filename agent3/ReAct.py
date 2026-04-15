980f5d90f5a7ee273b613eba3427370fc4c35825
Saif Ullah <saifu0905@gmail.com>
2026-04-15 11:31:01 +0500

    Imports plus defining what Annotated does

---
+++ agent3/ReAct.py
@@ -0,0 +1,20 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage
+from langchain_core.messages import ToolMessage
+from langchain_core.messages import SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+
+
+# Annotated - provides additional context without affecting the type itself
+
+# for example we define a email as str, it has a specific format. now if a person writes abc-gmail.com ,  It is not a 
+# valid email. That is where Annotated comes in.
+
+email = Annotated[str, "This has to be a valid email format!"]
+print(email.__metadata__)
