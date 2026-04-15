278ef6fc16bc3ae4d6f15c290c907d80d6611543
Saif Ullah <saifu0905@gmail.com>
2026-04-15 12:25:33 +0500

    Fullt functioning code with one Tool

--- agent3/ReAct.py
+++ agent3/ReAct.py
@@ -45,4 +45,69 @@ class AgentState(TypedDict):
     messages:  Annotated[Sequence[BaseMessage], add_messages] # It says to use Annotaed Sequence of Base Messages that have a reducer add_message
 
 
+@tool
+def add(a: int, b: int):
+    """This is an addition function that adds two numbers together"""
+    
+    return a + b
 
+
+tools = [add]
+
+model = ChatOllama(model="llama3.2").bind_tools(tools)
+
+
+def model_call(state: AgentState) -> AgentState:
+    # system_prompt = SystemMessage(content="You are my AI Assistant, please answer my query to the best of your ability")
+    # response = model.invoke(system_prompt + state["messages"])
+    
+    # Same thing as above in comments
+    response = model.invoke(["You are my AI Assistant, please answer my query to the best of your ability"] + state["messages"])
+    return {"messages": [response]}
+
+
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+            return "end"
+    else:
+         return "continue"
+    
+
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges("our_agent", should_continue,{
+     "continue": "tools",
+     "end": END
+})
+# go either from agent to tool or agent to END
+
+
+graph.add_edge("tools", "our_agent") # goes back from tools to agent, make it receursive, look in Graph of ReactAgent
+
+
+app = graph.compile()
+
+
+
+
+
+def print_stream(stream): # Just a function to beautifully print the whole process
+     for s in stream:
+          message = s["messages"][-1]
+          if isinstance(message, tuple):
+            print(tuple)
+          else:
+            message.pretty_print()
+
+
+inputs = {"messages": [("user", "Add 3 + 4")]}
+print_stream(app.stream(inputs, stream_mode="values"))
